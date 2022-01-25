2cef372491db2027ca04942b5773b2da0f58309a
DanXaPd <92036667+DanXaPd@users.noreply.github.com>
2022-01-25 23:53:49 +0200

    begginer_les32

---
+++ main.js
@@ -0,0 +1,184 @@
+//Begginer
+/*let a = +prompt ("Введите сторону квадрата")
+let b = a * 4
+alert("P квадарата " + b)
+console.log(b)*/
+
+
+//Begginer 2
+/*let a = +prompt ("Введите сторону квадрата")
+let b = a**2
+alert("S квадрата: " + b)
+console.log(b)*/
+
+
+//Begginer 3
+/*let a = +prompt("Введите длину прямоугольника")
+let b = +prompt("Введите ширину прямоугольника")
+let s = a*b
+let p = 2*(a+b)
+alert("S прямоугольника составляет: " + s + "   P прямоугольника составляет: " + p)
+console.log(s)
+console.log(p)*/
+
+
+//Beginer 4
+/*let d = +prompt("Введите диаметр окружности")
+let l = 3.14*d
+alert("Длина окружности равна " + l)
+console.log(l)*/
+
+
+//Begginer 5
+/*let a = +prompt("Введите значение ребра куба")
+let v = a**3 
+let s = 6*(a**2)
+alert("V куба равен: " + v + ";" + "   S куба равен: " + s +".")
+console.log(v)
+console.log(s)*/
+
+
+//Begginer 6
+/*let a = +prompt("Введите длину параллепипеда")
+let b = +prompt("Введите ширину параллепипеда")
+let c = +prompt("Введите высоту параллепипеда")
+let v = a*b*c
+let s = 2*((a*b)+(b*c)+(a*c))
+alert("V параллепипеда равен: " + v + ";" + "   S параллепипеда равна: " + s + ".")
+console.log(v)
+console.log(s)*/
+
+
+//Begginer 7
+/*let r = +prompt("Введите радиус окружности")
+let l = 2*3.14*r
+let s = 3.14*(r**2)
+alert("L окружности равна: " + l + "; " + "   S окружности равна: " + s +".")
+console.log(l)
+console.log(s)*/
+
+
+//Begginer 8
+/*let a = +prompt("Введите первое число")
+let b = +prompt("Введите второе число")
+let c = (a+b)/2
+alert("Среднее арефметическое данных чисел: " + c)
+console.log(c)*/
+
+
+//Begginer 9
+/*let a = +prompt("Введите первое число")
+let b = +prompt("Введите второе число")
+let c = (a*b)**0.5
+alert("Среднее геометрическое данных числел: " + c)
+console.log(c)*/
+
+
+//Begginer 10
+/*let a = +prompt ("Введите число")
+let b = +prompt ("Введите второе число")
+let c = (a**2)+(b**2)
+let d = (a**2)-(b**2)
+let e = (a**2)*(b**2)
+let f = (a**2)/(b**2)
+alert(" Сумма квадратов:" + c + " Разность квадаратов" + d + " Произведение квадратов" + e + " Частное квадратов" + f) 
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)*/
+
+
+//Begginer 11
+/*let a = +prompt ("Введите число")
+let b = +prompt ("Введите второе число")
+let c = ((a**2)**0.5)+((b**2)**0.5)
+let d = ((a**2)**0.5)-((b**2)**0.5)
+let e = ((a**2)**0.5)*((b**2)**0.5)
+let f = ((a**2)**0.5)/((b**2)**0.5)
+alert(" Сумма модулей:" + c + " Разность модулей:" + d + " Произведение модулей:" + e + " Частное модулей" + f) 
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)*/
+
+
+//Begginer 12
+/*let a = +prompt("Введите первый катет")
+let b = +prompt("Введите второй катет")
+let gipotinyza = ((a**2)+(b**2))**0.5
+let c = gipotinyza
+let p = a+b+c
+alert("Гипотенуза равна: " + c + ";" + "   P равен: " + p + ".")
+console.log(c)
+console.log(p)*/
+
+
+//Begginer 13
+/*alert("При решении данной задачи радиус первого круга должен быть больше радиуса второго")
+let r1 = +prompt("Введите радиус первого круга")
+let r2 = +prompt("Введите радиус второго круга круга")
+let s1 = 3.14*(r1**2)
+let s2 = 3.14*(r2**2)
+let s3 = s1-s2
+alert("S первого круга: " + s1 + ";" + "    S второго круга: " + s2 + ";" + "    S кольца: " + s3 + ".")
+console.log(s1)
+console.log(s2)
+console.log(s3)*/
+
+
+//Begginer 14
+/*let l = +prompt("Введите длинну окружности")
+let r = l/(2*3.14)
+let s = 3.14*(r**2)
+alert("Радиус окружности: " + r + ";" + "   Её площадь: " + s + ".")
+console.log(r)
+console.log(s)*/
+
+
+//Begginer 15
+/*let s = +prompt("Введите S круга")
+let d = ((4*s)/3.14)**0.5
+let l = 3.14*d
+alert("D круга: " + d + ";" + "   L круга: " + l + ".")
+console.log(d)
+console.log(l)*/
+
+
+//Begginer 25
+/*let x = prompt("Введите x")
+let y = 3*(x**6) - 6*(x**2) - 7
+alert(y)
+console.log(y)*/
+
+
+//Begginer 26
+/*let x = +prompt("Введите значение аргумента")
+let y = (4*((x-3)**6))-(7*((x-3)**3))-2
+alert("Значение функции: " + y)
+console.log(y)*/
+
+
+//Begginer 27
+/*let a = +prompt("Введите число")
+let b = a**2
+let с = a**4
+let d = a**8
+alert("Данное число во 2 степени: " + b + ";" + "   Данное число в 4 степени: " + c + ";" + "   Данное число в 8 степени: " + d + ".")
+console.log(b)
+console.log(c)
+console.log(d)*/
+
+
+//Begginer 28
+/*let a = +prompt("Введите число")
+let b = a**2
+let с = a**3
+let d = a**5
+let e = a**10
+let f = a**15
+alert("Данное число во 2 степени: " + b + ";" + "   Данное число в 3 степени: " + c + ";" + "   Данное число в 5 степени: " + d + ";" + "Данное число в 3 степени: " + e + ";" + "    Данное число в 5 степени: " + f + ".")
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)*/
